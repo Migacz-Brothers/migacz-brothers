a5bee554b68387957971d96052c2a95c368ee403
Pedro H G Migacz <pedromigacz@gmail.com>
2022-11-25 03:16:38 -0300

    add babel to SSR styled-components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,39 +1,41 @@
-import * as React from 'react';
 import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
   DocumentContext,
   DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
 } from 'next/document';
-import { AppType } from 'next/dist/shared/lib/utils';
+import { ServerStyleSheet } from 'styled-components';
 
-interface DocumentProps extends DocumentInitialProps {
-  emotionStyleTags: React.ReactNode[];
+import { renderToString } from 'react-dom/server';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
 }
-
-const MyDocument = (props: DocumentProps) => {
-  return (
-    <Html lang='en'>
-      <Head>
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link
-          rel='preconnect'
-          href='https://fonts.gstatic.com'
-          crossOrigin='true'
-        />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap'
-          rel='stylesheet'
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-};
-
-export default MyDocument;
