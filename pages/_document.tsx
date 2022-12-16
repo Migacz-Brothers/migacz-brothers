a59bfe147225e7103b826473c972f9422470c94c
Pedro H G Migacz <pedromigacz@gmail.com>
2022-12-16 01:27:40 -0300

    add cloudflare analytics script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -53,6 +53,11 @@ export default class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap'
             rel='stylesheet'
           />
+          <script
+            defer
+            src='https://static.cloudflareinsights.com/beacon.min.js'
+            data-cf-beacon='{"token": "e8c4440ab3b6434790a21f11e563615f"}'
+          ></script>
         </Head>
         <body>
           <Main />
