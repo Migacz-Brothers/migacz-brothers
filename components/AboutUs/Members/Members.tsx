77364647abfa57e223d7a3ad480e2a01b56490fd
Pedro H G Migacz <pedromigacz@gmail.com>
2022-12-20 02:24:44 -0300

    add members Arthur and Iago

--- components/AboutUs/Members/Members.tsx
+++ components/AboutUs/Members/Members.tsx
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 import { Invitation, Member } from '.';
 import Gustavo from '../../../public/members/gustavo.jpeg';
 import Pedro from '../../../public/members/pedro.jpg';
+import Iago from '../../../public/members/iago.jpeg';
+import Arthur from '../../../public/members/arthur.jpg';
 import Brenno from '../../../public/members/brenno.jpg';
 
 const Members = (): JSX.Element => {
@@ -12,6 +14,18 @@ const Members = (): JSX.Element => {
         position='Co-Founder'
         description='Web developer with 5+ years of experience in web development, Pedro has a deep understanding of creating systems from responsive websites to complex backend systems.'
         picture={Pedro}
+      />
+      <Member
+        name='Iago M. Pzivitovski'
+        position='Engineer'
+        description='Iago is a great frontend developer, and a big part of our team. He has extensive experience in the industry and a passion for problem solving.'
+        picture={Iago}
+      />
+      <Member
+        name='Arthur V. Franceschini'
+        position='Marketing Director'
+        description='With knowledge both in marketing and development, he brings a wealth of experience to the organization. He is passionate about creating innovative and successful campaigns that will help drive growth.'
+        picture={Arthur}
       />
       <Member
         name='Gustavo G. Migacz'
