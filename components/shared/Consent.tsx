e81e119cf29a8d302bd0a334e7862f2facaa6456
Pedro H G Migacz <pedromigacz@gmail.com>
2022-12-23 12:32:44 -0300

    add Consent popup

---
+++ components/shared/Consent.tsx
@@ -0,0 +1,78 @@
+import { AnimatePresence, motion } from 'framer-motion';
+import React, { useEffect, useState } from 'react';
+
+import styled from 'styled-components';
+
+function Consent() {
+  const [consent, setConsent] = useState(false);
+  // useEffect(() => {
+  //   setConsent(hasCookie('localConsent'));
+  // }, []);
+
+  const acceptCookie = () => {
+    setConsent(true);
+    // setCookie('localConsent', 'true', { maxAge: 60 * 60 * 24 * 365 });
+  };
+
+  return (
+    <AnimatePresence>
+      {!consent ? (
+        <Modal
+          key='cookie_consent_modal'
+          initial={{
+            x: 600,
+          }}
+          animate={{
+            x: 0,
+          }}
+          exit={{
+            x: 600,
+          }}
+        >
+          We use cookies to improve our website and assist with our marketing
+          campaigns. By clicking &quot;Accept Cookies&quot;, you consent to the
+          storing of cookies on your device.
+          <div className='flex align-middle justify-between'>
+            <Button
+              onClick={() => {
+                acceptCookie();
+              }}
+              className='p-2 bg-green-400 rounded-md'
+            >
+              Accept All
+            </Button>
+          </div>
+        </Modal>
+      ) : null}
+    </AnimatePresence>
+  );
+}
+
+const Modal = styled(motion.div)`
+  position: fixed;
+  background-color: #101010;
+  border-radius: var(--rounded);
+  color: var(--light-primary);
+  bottom: 8px;
+  right: 8px;
+  margin-left: 8px;
+  padding: 32px 48px;
+  max-width: 500px;
+  box-shadow: 0px 4px 9px rgba(255, 255, 255, 0.02);
+
+  display: block;
+`;
+
+const Button = styled.button`
+  background-color: var(--primary);
+  padding: 8px 24px;
+  border-radius: var(--rounded);
+  display: block;
+  margin-left: auto;
+
+  @media (max-width: 500px) {
+    margin-top: 12px;
+  }
+`;
+
+export default Consent;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,11 @@ import type { AppProps } from 'next/app';
 import GlobalStyle from '../components/styles/globalStyles';
 import { Navbar, Footer } from '../components/shared';
 import ThemeContextProvider from '../components/Contexts/ThemeContext';
+import dynamic from 'next/dynamic';
+
+const Consent = dynamic(() => import('../components/shared/Consent'), {
+  suspense: true,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Navbar />
       <Component {...pageProps} />
       <Footer />
+      <Consent />
     </ThemeContextProvider>
   );
 }
