b200f48125a2fe6024beefa482b51a696b37c5af
Pedro H G Migacz <pedromigacz@gmail.com>
2023-01-02 16:58:35 -0300

    update path properties

--- components/Icons/TechStack/companies/Nasa.tsx
+++ components/Icons/TechStack/companies/Nasa.tsx
@@ -5,7 +5,7 @@ const NasaIcon = (): JSX.Element => (
     fill='none'
     xmlns='http://www.w3.org/2000/svg'
   >
-    <g clip-path='url(#clip0_1861_468)'>
+    <g clipPath='url(#clip0_1861_468)'>
       <path
         fillRule='evenodd'
         clipRule='evenodd'
