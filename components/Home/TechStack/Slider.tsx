333257f51e57206014c4890a0f5ae2864fa646a7
Pedro H G Migacz <pedromigacz@gmail.com>
2023-01-02 02:45:57 -0300

    add basic content on tech stack component

--- components/Home/TechStack/Slider.tsx
+++ components/Home/TechStack/Slider.tsx
@@ -28,35 +28,35 @@ const TechStackList = [
   {
     title: 'Node',
     paragraph:
-      'odeJS is a powerful and reliable tool for writing backend code. This is the basis for many of our services including APIs, CMS, custom systems, Discord bots, etc.',
+      'NodeJS is a powerful and reliable tool for writing backend code. This is the basis for many of our services including APIs, CMS, custom systems, Discord bots, etc.',
     icon: NodeIcon,
     socialProof: [MediumIcon, NetflixIcon],
   },
   {
     title: 'PostgreSQL',
     paragraph:
-      'NodeJS is a powerful and reliable tool for writing backend code. This is the basis for many of our services including APIs, CMS, custom systems, Discod bots, etc.',
+      "PostgreSQL is a feature-rich, open-source database management system. It's popular for its support of complex SQL queries, full-text search, and powerful indexing options. We use it to store and manage data for your web apps.",
     icon: PostgreSqlIcon,
     socialProof: [RobinhoodIcon, NasaIcon],
   },
   {
     title: 'Next',
     paragraph:
-      'NodeJS is a powerful and reliable tool for writing backend code. This is the basis for many of our services including APIs, CMS, custom systems, Discod bots, etc.',
+      "NextJS is what makes our websites so blazing fast. It's our Server Side Rendering engine of choice for React. It offers automatic code splitting and optimized performance for fast, scalable web applications.",
     icon: NextIcon,
     socialProof: [TypeformIcon, NotionIcon],
   },
   {
     title: 'Sanity',
     paragraph:
-      'NodeJS is a powerful and reliable tool for writing backend code. This is the basis for many of our services including APIs, CMS, custom systems, Discod bots, etc.',
+      'Sanity is a flexible headless CMS that makes it easy to manage and deliver structured content. This technology allows us developers to create complex relationships while keeping it extremely simple for you (the client) to manage your content.',
     icon: SanityIcon,
     socialProof: [NetlifyIcon, NikeIcon],
   },
   {
     title: 'Figma',
     paragraph:
-      'NodeJS is a powerful and reliable tool for writing backend code. This is the basis for many of our services including APIs, CMS, custom systems, Discod bots, etc.',
+      'Figma is a powerful design and prototyping tool that helps us create, share, and collaborate on designs with you. With Figma, we can provide you with wireframes, prototypes, and fully-designed screens all in one place.',
     icon: FigmaIcon,
     socialProof: [AirbnbIcon, ZoomIcon],
   },
