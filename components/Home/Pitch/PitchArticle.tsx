d0dbf33bc0e6cbd7292ad54d79b32411dc17df04
Pedro H G Migacz <pedromigacz@gmail.com>
2023-01-07 03:09:07 -0300

    add mobile version

--- components/Home/Pitch/PitchArticle.tsx
+++ components/Home/Pitch/PitchArticle.tsx
@@ -21,9 +21,17 @@ const PitchArticle = ({
           <Title>{title}</Title>
           <Paragraph>{description}</Paragraph>
         </DataContainer>
-        <aside>
-          <Image src={image} alt={`${title} ilustration`} quality={100} />
-        </aside>
+        <Aside>
+          <CustomImage
+            src={image}
+            alt={`${title} ilustration`}
+            quality={100}
+            fill
+            sizes='(max-width: 520px) 100vw,
+                  489px'
+            placeholder='blur'
+          />
+        </Aside>
       </ArticleInnerContainer>
     </Article>
   );
@@ -34,6 +42,10 @@ const Article = styled.article`
   margin: 0 auto;
   margin-bottom: 160px;
   padding: 0 16px;
+
+  @media (max-width: 980px) {
+    margin-bottom: 100px;
+  }
 `;
 
 interface ArticleInnerContainerProps {
@@ -46,10 +58,21 @@ const ArticleInnerContainer = styled.div<ArticleInnerContainerProps>`
   gap: 100px;
   justify-content: space-between;
   align-items: center;
+
+  @media (max-width: 980px) {
+    gap: 48px;
+  }
+
+  @media (max-width: 700px) {
+    flex-direction: column-reverse;
+    gap: 0;
+  }
 `;
 
 const DataContainer = styled.div`
+  /* @media (max-width: 980px) { */
   /* max-width: 600px; */
+  /* } */
 `;
 
 const Title = styled.h2`
@@ -57,12 +80,48 @@ const Title = styled.h2`
   font-size: 32px;
   /* line-height: 80px; */
   padding: 20px 0 21px 0;
+
+  @media (max-width: 980px) {
+    padding: 16px 0 16px 0;
+    font-size: 28px;
+  }
+
+  /* @media (max-width: 700px) {
+    text-align: center;
+  } */
 `;
 
 const Paragraph = styled.p`
   color: var(--text-secondary);
   line-height: 150%;
   font-size: 22px;
+
+  @media (max-width: 980px) {
+    font-size: 18px;
+  }
+
+  /* @media (max-width: 700px) {
+    text-align: center;
+  } */
+`;
+
+const Aside = styled.aside`
+  position: relative;
+  min-width: 489px;
+  display: inline-block;
+  /* max-width: 50vw; */
+
+  @media (max-width: 980px) {
+    min-width: 380px;
+  }
+
+  @media (max-width: 700px) {
+    min-width: calc(100vw - 32px);
+  }
+`;
+
+const CustomImage = styled(Image)`
+  position: relative !important;
 `;
 
 export default PitchArticle;
