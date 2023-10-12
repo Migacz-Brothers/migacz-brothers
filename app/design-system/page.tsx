8ec29c02060fffe65f72d8cb4f76cf76080f3e06
Pedro H G Migacz <pedromigacz@gmail.com>
2023-10-12 01:48:51 -0300

    improve basic typography

--- app/design-system/page.tsx
+++ app/design-system/page.tsx
@@ -1,17 +1,20 @@
-import { h1, h2, h3 } from '@/components/typography';
+import { h1, h2, h3, p } from '@/components/typography';
 
 export default function Home() {
   return (
     <main className='light-noise-background'>
-      <h1 className={h1}>
-        Header1: Helping companies create unique digital products.
-      </h1>
-      <p className='font-body text-main text-sm md:text-base leading-body'>
-        Paragraph: Get access to high-end designers, frontend and backend
-        developers, branding specialists, and much more in a single place!
-      </p>
-      <h2 className={h2}>Header2: About Us</h2>
-      <h3 className={h3}>DietIt Project</h3>
+      <section className='max-w-[1232px] md:px-4 px-8 mx-auto'>
+        <h1 className={h1}>
+          Header1: Helping companies create unique{' '}
+          <strong className='gradient-bold'>digital products</strong>.
+        </h1>
+        <p className={p}>
+          Paragraph: Get access to high-end designers, frontend and backend
+          developers, branding specialists, and much more in a single place!
+        </p>
+        <h2 className={h2}>Header2: About Us</h2>
+        <h3 className={h3}>DietIt Project</h3>
+      </section>
     </main>
   );
 }
--- components/typography.ts
+++ components/typography.ts
@@ -1,3 +1,4 @@
 export const h1 = 'font-header text-main leading-header text-3xl md:text-5xl';
 export const h2 = 'font-header text-main leading-header text-2xl md:text-4xl';
 export const h3 = 'font-header text-main leading-header text-lg md:text-2xl';
+export const p = 'font-body text-main text-sm md:text-base leading-body';
