67b83212c28bfc2af4a14009534ae9e10e5bcc88
Pedro H G Migacz <pedromigacz@gmail.com>
2023-10-12 15:33:09 -0300

    add basic hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
+import Hero from '@/components/Hero';
+
 export default function Home() {
   return (
     <main>
-      <section className='light-noise-background bg-repeat h-96'>
-        <h1 className='font-header'>Hello world</h1>
-        <p className='font-body'>Hello world paragraph</p>
-      </section>
+      <Hero />
       <section className='blue-noise-background h-96'>
         <h1 className='font-header'>Hello world</h1>
         <p className='font-body'>Hello world paragraph</p>
---
+++ components/Hero.tsx
@@ -0,0 +1,26 @@
+import { h1, p, section } from '@/components/design-system';
+import cn from 'clsx';
+
+export default function Hero() {
+  return (
+    <div className='light-noise-background'>
+      <section className={section}>
+        <div className='max-w-[652px] flex flex-col'>
+          <span className={cn(p, 'uppercase underline mb-3')}>
+            MigaczBrothers
+          </span>
+          <h1 className={cn(h1, 'mb-6')}>
+            Helping companies create unique{' '}
+            <strong className='gradient-bold'>digital products</strong>.
+          </h1>
+          <p className={cn(p, 'font-medium mb-16 max-w-[434px]')}>
+            Get access to high-end designers, frontend and backend developers,
+            branding specialists, and much more in a{' '}
+            <strong className='font-medium underline'>single place</strong>!
+          </p>
+        </div>
+        <aside></aside>
+      </section>
+    </div>
+  );
+}
