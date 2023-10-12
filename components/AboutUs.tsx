02fc8b4cc427e7dbfa5b49b019c66adcc7d88ac5
Gustavo G Migacz <gustavomigacz@gmail.com>
2023-10-12 20:38:45 -0300

    add AboutUs

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -8,21 +8,31 @@ import Image from 'next/image';
 import Link from 'next/link';
 import cn from 'clsx';
 import { ReactNode } from 'react';
+import { h2, p } from './design-system';
 
 export default function AboutUs() {
   return (
     <section>
-      <div className='max-w-[1264px] mx-auto px-4'>
+      <div className='max-w-[1264px] mx-auto lg:px-4 px-8 flex lg:flex-row flex-col justify-between ld:gap-8 gap-4 items-baseline lg:items-center w-fit lg:w-full'>
         <Image src={AboutUsImage} alt={'A photo about group pro working'} />
-        <div>
-          <h2>About Us</h2>
-          <h3>Design & Code</h3>
-          <p>
-            We are a small cooperative company from South Brazil with an
-            international team of professionals who have joined forces to create
-            unique products for the web.
-          </p>
-          <ul className='flex md:gap-2 gap-[6px] pt-2'>
+        <div className='max-w-[446px]'>
+          <div>
+            <h2 className={cn(h2, '-mb-[2px]')}>About Us</h2>
+            <h3
+              className={
+                'font-header text-main leading-header text-base md:text-lg'
+              }
+            >
+              Design & Code
+            </h3>
+            <p className={cn(p, 'mt-2 lg:mt-[24px] lg:mb-[41px] ')}>
+              We are a small cooperative company from South Brazil with an
+              international team of professionals who have joined forces to
+              create unique products for the web.
+            </p>
+          </div>
+
+          <ul className='flex lg:gap-2 gap-[6px] pt-2'>
             <SocialLink className='bg-[#3367CC]'>
               <BehanceIcon />
             </SocialLink>
@@ -58,7 +68,7 @@ const SocialLink = ({ children, className, text }: SocialLinkProps) => {
   return (
     <li
       className={cn(
-        'rounded-[4px] font-body text-xs md:text-base leading-[12px]',
+        'rounded-[4px] font-body text-xs lg:text-base leading-[12px]',
         className
       )}
     >
@@ -66,7 +76,7 @@ const SocialLink = ({ children, className, text }: SocialLinkProps) => {
         href={'#'}
         className='w-full h-full flex flex-row gap-1 items-center'
       >
-        <div className='scale-75 md:scale-100 w-10 h-10 md:w-[54px] md:h-[54px] rounded-[4px] grid place-items-center'>
+        <div className='scale-75 xl:scale-100 w-10 h-10 xl:w-[54px] xl:h-[54px] rounded-[4px] grid place-items-center'>
           {children}
         </div>
         {text}
