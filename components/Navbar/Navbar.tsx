712cb7d430490b928ee87f7a818ef6d05d8c72d4
Gustavo G Migacz <gustavomigacz@gmail.com>
2023-10-16 16:08:21 -0300

    add Desktop navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -5,24 +5,32 @@ import NavbarIcon from '../svgs/NavbarIcon';
 
 export default function Navbar() {
   return (
-    <nav>
-      <div className='max-w-[1264px] mx-auto lg:px-4 px-8 flex justify-between my'>
-        <div className='cursor-pointer'>
-          <NavbarIcon />
+    <>
+      {/* nav desktop */}
+      <nav className='w-full bg-[#ffffff10] fixed z-50 top-0 left-0 py-6'>
+        <div className='max-w-[1264px] mx-auto lg:px-4 px-8 flex justify-between'>
+          <div className=''>
+            <NavbarIcon />
+          </div>
+          <ul className='flex items-center gap-12'>
+            <NavLinks>Home</NavLinks>
+            <NavLinks>About Us</NavLinks>
+            <NavLinks>Portofolio</NavLinks>
+            <Link
+              href={'#'}
+              className='w-[178px] h-10 nav-background grid place-items-center rounded text-lg font-semibold'
+            >
+              Contact Us
+            </Link>
+          </ul>
         </div>
-        <ul className='flex items-center gap-12'>
-          <NavLinks>Home</NavLinks>
-          <NavLinks>About Us</NavLinks>
-          <NavLinks>Portofolio</NavLinks>
-          <Link
-            href={''}
-            className='w-[178px] h-10 nav-background grid place-items-center rounded text'
-          >
-            Contact Us
-          </Link>
-        </ul>
-      </div>
-    </nav>
+      </nav>
+      {/* nav desktop */}
+
+      {/* nav mobile */}
+      <div></div>
+      {/* nav mobile */}
+    </>
   );
 }
 
@@ -34,11 +42,11 @@ const NavLinks = ({ children }: NavLinksProps) => {
   return (
     <li>
       <Link
-        href={''}
-        className='text-[#ffffff] font-header hover:font-bold font-medium text-lg'
+        href={'#'}
+        className='inline-block text-[#ffffff] font-header font-medium text-lg group'
       >
         {children}
-        <div className='h-[2px] w-full nav-background rounded-[50px]' />
+        <div className='h-[2px] w-full nav-background rounded-[50px] opacity-0 group-hover:opacity-100 duration-200' />
       </Link>
     </li>
   );
