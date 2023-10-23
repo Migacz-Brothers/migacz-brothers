5ea2157ec87b9d103429f67d17a1007f11127b12
Pedro H G Migacz <pedromigacz@gmail.com>
2023-10-23 16:42:10 -0300

    add hover effect back to Tags

--- components/Tags.tsx
+++ components/Tags.tsx
@@ -17,9 +17,10 @@ export const Tags = ({ tagList, title = '' }: TagsProps) => (
         viewport={{ once: true, amount: 0.1 }}
         transition={{ duration: 0.4, ease: 'easeInOut', delay: 0.05 + 0.1 * i }}
         key={`tag-${i}-${tag}-${title}`}
-        className='font-header text-main leading-header text-xs md:text-base px-[12px] md:px-[25px] py-[3px] md:py-[5px] rounded md:rounded-lg bg-tag backdrop-blur-xsm outline outline-1 md:outline-2 -outline-offset-1 md:-outline-offset-1 group-hover:bg-tag-hover'
       >
-        {tag}
+        <motion.span className='font-header text-main leading-header text-xs md:text-base px-[12px] md:px-[25px] py-[3px] md:py-[5px] rounded md:rounded-lg bg-tag backdrop-blur-xsm outline outline-1 md:outline-2 -outline-offset-1 md:-outline-offset-1 group-hover:bg-tag-hover duration-300'>
+          {tag}
+        </motion.span>
       </motion.li>
     ))}
   </ul>
