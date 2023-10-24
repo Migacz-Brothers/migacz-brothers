bd794379f95c8e9cd3622b613cbcaac814fb6869
Pedro H G Migacz <pedromigacz@gmail.com>
2023-10-24 17:37:51 -0300

    update Footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -25,7 +25,7 @@ export default function Footer({ home = false }: FooterProps) {
         transition={{ duration: 0.3, ease: 'easeInOut' }}
       >
         <div className='flex lg:flex-row flex-col justify-between lg:items-center items-start mb-6 gap-8'>
-          <a href={'/#home'} className='order-none'>
+          <a href={(home ? '' : '/') + '#home'} className='order-none'>
             <CompanyLogo className='w-[99px] h-[41px]' />
           </a>
           <ul className='flex gap-8 lg:order-none order-3'>
@@ -74,10 +74,16 @@ export default function Footer({ home = false }: FooterProps) {
             <LanguageLink href='/es'>ES</LanguageLink>
           </ul>
           <ul className='flex lg:gap-8 gap-4 lg:flex-row flex-col lg:order-none order-1'>
-            <FooterLink href='#home'>Home</FooterLink>
-            <FooterLink href='#about'>About Us</FooterLink>
-            <FooterLink href='#portfolio'>Portofolio</FooterLink>
-            <FooterLink href='#portfolio'>Contact Us</FooterLink>
+            <FooterLink href={(home ? '' : '/') + '#home'}>Home</FooterLink>
+            <FooterLink href={(home ? '' : '/') + '#about'}>
+              About Us
+            </FooterLink>
+            <FooterLink href={(home ? '' : '/') + '#portfolio'}>
+              Portofolio
+            </FooterLink>
+            <FooterLink href={(home ? '' : '/') + '#portfolio'}>
+              Contact Us
+            </FooterLink>
           </ul>
         </div>
         <motion.hr
@@ -91,7 +97,7 @@ export default function Footer({ home = false }: FooterProps) {
         />
         <div className='lg:text-right text-start font-body text-sm'>
           <div className='font-body text-main mb-1'>
-            Migacz Brothers © Todos os direitos reservados.
+            MigaczBrothers © All rights reserved.
           </div>
           <div className='text-[#7C7C7C]'>CNPJ 48.546.424/0001-82</div>
         </div>
