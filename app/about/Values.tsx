04cbee4b75def2cd258752543e76518780ce396c
Pedro H G Migacz <pedromigacz@gmail.com>
2023-10-31 14:47:11 -0300

    animate layout

--- app/about/Values.tsx
+++ app/about/Values.tsx
@@ -1,13 +1,23 @@
+'use client';
+
+import cn from 'clsx';
+import { motion } from 'framer-motion';
+
 import { h2, p, section } from '@/components/design-system';
 import HeartHandShakeIcon from '@/components/svgs/HeartHandShakeIcon';
 import HelpCircleIcon from '@/components/svgs/HelpCircleIcon';
 import ShieldCheckIcon from '@/components/svgs/ShieldCheckIcon';
 import SpeetchIcon from '@/components/svgs/SpeetchIcon';
-import cn from 'clsx';
 
 export default function Values() {
   return (
-    <section
+    <motion.section
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ once: true, amount: 0.1 }}
+      transition={{ duration: 0.4, ease: 'easeInOut' }}
       className={cn(section, 'flex flex-col items-start !max-w-[916px]')}
     >
       <h2 className={cn(h2, 'md:mb-16 mb-4')}>The Values</h2>
@@ -18,6 +28,7 @@ export default function Values() {
           paragraph={
             'We believe the best and mot authentic work happens when everyone is free to speak about anything at anytime.'
           }
+          delay={0.2}
         />
         <IndividualValue
           icon={<HeartHandShakeIcon />}
@@ -25,6 +36,7 @@ export default function Values() {
           paragraph={
             'We create everything with long-term maintenance in mind. Once you become our client, we make a commitment to never let you down.'
           }
+          delay={0.3}
         />
         <IndividualValue
           icon={<HelpCircleIcon />}
@@ -32,6 +44,7 @@ export default function Values() {
           paragraph={
             'Everything changes, and it happens faster on this industry. We say yes to projects even if we are not sure how to do some parts of it.'
           }
+          delay={0.4}
         />
         <IndividualValue
           icon={<ShieldCheckIcon />}
@@ -39,9 +52,10 @@ export default function Values() {
           paragraph={
             'Every product we create has to be approved by our team. We wanna be know for our quality, and that`s our main goal.'
           }
+          delay={0.5}
         />
       </ul>
-    </section>
+    </motion.section>
   );
 }
 
@@ -49,11 +63,24 @@ interface IndividualValueProps {
   icon: JSX.Element;
   title: string;
   paragraph: string;
+  delay: number;
 }
 
-const IndividualValue = ({ icon, title, paragraph }: IndividualValueProps) => {
+const IndividualValue = ({
+  icon,
+  title,
+  paragraph,
+  delay,
+}: IndividualValueProps) => {
   return (
-    <li>
+    <motion.li
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ once: true, amount: 0.1 }}
+      transition={{ duration: 0.4, ease: 'easeInOut', delay: delay }}
+    >
       <div className='grid place-items-center hero-background rounded-lg mb-4 md:mb-8 w-[50px] md:w-16 h-[50px] md:h-16'>
         {icon}
       </div>
@@ -63,6 +90,6 @@ const IndividualValue = ({ icon, title, paragraph }: IndividualValueProps) => {
       <p className={cn(p, '!text-main-foreground font-medium leading-body')}>
         {paragraph}
       </p>
-    </li>
+    </motion.li>
   );
 };
