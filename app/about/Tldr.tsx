c983d428c4bc1fdedfe154f6eb1acbbffcd53e10
Pedro H G Migacz <pedromigacz@gmail.com>
2023-11-02 20:20:24 -0300

    update TLDR structure

--- app/about/Tldr.tsx
+++ app/about/Tldr.tsx
@@ -17,18 +17,16 @@ export default function Tldr() {
       className={section}
     >
       <h2 className={cn(h2, 'md:mb-6 mb-4')}>The tl;dr</h2>
-      <div className='max-w-[514px] flex flex-col gap-4'>
-        <p className={p}>
-          We know you are a busy person, so we&apos;ll get straight to the
-          point. We&apos;re a cooperative of designers and software developers
-          who united to create high-quality products that might otherwise be
-          impracticable. Our team is global, with our company&apos;s main office
-          located in South Brazil.
-        </p>
-        <p className={cn(p, 'flex gap-2')}>
-          If you wanna learn more, just keep scrolling <ScrollAnimated />
-        </p>
-      </div>
+      <p className={cn(p, 'max-w-[514px] mb-4')}>
+        We know you are a busy person, so we&apos;ll get straight to the point.
+        We&apos;re a cooperative of designers and software developers who united
+        to create high-quality products that might otherwise be impracticable.
+        Our team is global, with our company&apos;s main office located in South
+        Brazil.
+      </p>
+      <p className={cn(p, 'flex gap-2 max-w-[514px]')}>
+        If you wanna learn more, just keep scrolling <ScrollAnimated />
+      </p>
     </motion.section>
   );
 }
