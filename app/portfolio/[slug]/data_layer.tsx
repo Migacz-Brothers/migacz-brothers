bfaf516c151a1e92d6706c7ef6855626ec4770a5
Pedro H G Migacz <pedromigacz@gmail.com>
2024-02-16 18:03:31 -0300

    remove tina

--- app/portfolio/[slug]/data_layer.tsx
+++ app/portfolio/[slug]/data_layer.tsx
@@ -1,35 +1,12 @@
 'use client';
 
-import { Exact, ProjectsQuery } from '@/tina/__generated__/types';
-import { useTina } from 'tinacms/dist/react';
-import { TinaMarkdown } from 'tinacms/dist/rich-text';
-
-interface PortfolioDataLayerProps {
-  variables: Exact<{
-    relativePath: string;
-  }>;
-  data_: ProjectsQuery;
-  query: string;
-}
-
-export default function PortfolioDataLayer({
-  variables,
-  data_,
-  query,
-}: PortfolioDataLayerProps) {
-  const {
-    data: { projects: data },
-  } = useTina({
-    variables: variables,
-    data: data_,
-    query: query,
-  });
-
+export default function PortfolioDataLayer({}) {
   return (
     <>
-      <h1>titulo {data.title}</h1>
-      <TinaMarkdown content={data.body} />
-      <p>{data.date}</p>
+      <h1>titulo title</h1>
+      <p>body</p>
+
+      <p>date</p>
     </>
   );
 }
--- app/portfolio/[slug]/page.tsx
+++ app/portfolio/[slug]/page.tsx
@@ -1,7 +1,3 @@
-import client from '@/tina/__generated__/client';
-import { useTina } from 'tinacms/dist/react';
-import { TinaMarkdown } from 'tinacms/dist/rich-text';
-
 import Navbar from '@/components/Navbar/Navbar';
 import PortfolioDataLayer from '@/app/portfolio/[slug]/data_layer';
 
@@ -10,19 +6,11 @@ export default async function ProjectPage({
 }: {
   params: { slug: string };
 }) {
-  const res = await client.queries.projects({
-    relativePath: `/${slug}.mdx`,
-  });
-
   return (
     <>
       <Navbar />
       <main className='pt-28 md:pt-40'>
-        <PortfolioDataLayer
-          variables={res.variables}
-          data_={res.data}
-          query={res.query}
-        />
+        <PortfolioDataLayer />
         <p>SKIBIDI PAPA</p>
         <p>SLUG {slug}</p>
       </main>
@@ -32,15 +20,15 @@ export default async function ProjectPage({
 
 // app/[...slug]/page.js
 
-export async function generateStaticParams() {
-  const projects = (await client.queries.projectsConnection()).data
-    .projectsConnection.edges;
+// export async function generateStaticParams() {
+//   const projects = (await client.queries.projectsConnection()).data
+//     .projectsConnection.edges;
 
-  if (!projects) return [];
+//   if (!projects) return [];
 
-  return projects.map((project) => {
-    return {
-      slug: project?.node?._sys.filename,
-    };
-  });
-}
+//   return projects.map((project) => {
+//     return {
+//       slug: project?.node?._sys.filename,
+//     };
+//   });
+// }
