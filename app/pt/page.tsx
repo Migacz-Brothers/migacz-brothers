044aaf80eeb9095496067df4cfac31c3eac364a2
Gustavo G Migacz <gustavomigacz@gmail.com>
2024-02-21 13:55:19 -0300

    add AboutUs in portuguese

---
+++ app/pt/page.tsx
@@ -0,0 +1,43 @@
+import AboutUs from '@/components/AboutUs';
+import ContactUs from '@/components/ContactUs/ContactUs';
+import Footer from '@/components/Footer/Footer';
+import Hero from '@/components/Hero';
+import Navbar from '@/components/Navbar/Navbar';
+import Portfolio from '@/components/Portfolio';
+
+export default function Home() {
+  return (
+    <>
+      <Navbar home />
+      <main>
+        <Hero
+          title={
+            <>
+              Ajudando empresas a criar produtos{' '}
+              <strong className='gradient-bold'>digitais únicos</strong>.
+            </>
+          }
+          paragraph={
+            <>
+              Tenha acesso a designers de alta qualidade, desenvolvedores
+              frontend e backend, especialistas em branding e muito mais em um{' '}
+              <strong className='font-medium underline'>único lugar</strong>
+            </>
+          }
+          textButton='Contacte-nos em'
+        />
+        <AboutUs
+          title={'Sobre nós'}
+          subTitle={'Design & Código'}
+          paragraph={
+            'Somos uma pequena empresa cooperativa do Sul do Brasil com uma equipe internacional de profissionais que uniram forças para criar produtos exclusivos para a web.'
+          }
+          text={'Ou leia mais'}
+        />
+        <Portfolio />
+        <ContactUs />
+      </main>
+      <Footer home />
+    </>
+  );
+}
