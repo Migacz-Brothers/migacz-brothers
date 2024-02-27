cd7092b8bf2e67e27efd251329a949ab2016ae62
Pedro H G Migacz <pedromigacz@gmail.com>
2024-02-27 01:35:09 -0300

    update background fuzzy animation

--- components/FuzzyBg.tsx
+++ components/FuzzyBg.tsx
@@ -5,20 +5,27 @@ import { motion } from 'framer-motion';
 const FuzzyOverlay = () => {
   return (
     <motion.div
-      initial={{ transform: 'translateX(-10%) translateY(-10%)' }}
+      initial={{ transform: 'translateX(-10%) translateY(-10%)', opacity: 0 }}
       animate={{
         transform: 'translateX(10%) translateY(10%)',
+        opacity: 0.1,
       }}
       transition={{
-        repeat: Infinity,
-        duration: 0.2,
-        ease: 'linear',
-        repeatType: 'mirror',
+        transform: {
+          repeat: Infinity,
+          duration: 0.2,
+          ease: 'linear',
+          repeatType: 'mirror',
+        },
+        opacity: {
+          duration: 0.5,
+          repeat: 0,
+        },
       }}
       style={{
         backgroundImage: 'url("/white_noise.png")',
       }}
-      className='pointer-events-none absolute -inset-[100%] opacity-10'
+      className='pointer-events-none absolute -inset-[100%] z-10 opacity-10'
     />
   );
 };
