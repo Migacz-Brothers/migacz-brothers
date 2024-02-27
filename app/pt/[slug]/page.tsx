4dee3e234c8981263571f088bc6cf1f6b8a87982
Pedro H G Migacz <pedromigacz@gmail.com>
2024-02-27 18:42:02 -0300

    dry and animate project pages

--- app/pt/[slug]/page.tsx
+++ app/pt/[slug]/page.tsx
@@ -1,14 +1,13 @@
-import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import { getAllProjects, getSpecificProject } from '@/sanity/lib/sanity';
 import cn from 'clsx';
-import { LogIn } from 'lucide-react';
 
-import { project_section, section } from '@/components/design-system';
+import { section } from '@/components/design-system';
 import Footer from '@/components/Footer/Footer';
 import Navbar from '@/components/Navbar/Navbar';
+import ProjectContent from '@/components/ProjectContent';
+import ProjectHeader from '@/components/ProjectHeader';
 import { Tags } from '@/components/Tags';
-import PortfolioDataLayer from '@/app/pt/[slug]/data_layer';
 
 export default async function ProjectPage({
   params: { slug },
@@ -42,7 +41,7 @@ export default async function ProjectPage({
           read_time={String(project.read_time?.pt)}
           link={project.link}
         />
-        <PortfolioDataLayer project={project} />
+        <ProjectContent project={project} lang='pt' />
         <Tags tagList={project.tags as string[]} />
       </main>
       <Footer
@@ -55,46 +54,6 @@ export default async function ProjectPage({
     </>
   );
 }
-
-interface ProjectHeaderProps {
-  title: string;
-  description: string;
-  executed_at: string;
-  read_time: string;
-  link?: string | null;
-}
-
-const ProjectHeader = ({
-  title,
-  description,
-  executed_at,
-  read_time,
-  link,
-}: ProjectHeaderProps) => {
-  return (
-    <div className={cn(project_section, 'mb-8')}>
-      <h1 className='font-header text-3xl font-semibold leading-header md:text-5xl'>
-        {title}
-      </h1>
-      <p className='font-header text-lg leading-header md:text-4xl'>
-        {description}
-      </p>
-      <p className='flex gap-3 text-sm font-light md:text-base'>
-        <span>{executed_at}</span>•<span>{read_time}</span>
-      </p>
-      {link ? (
-        <Link
-          href={link}
-          className='mt-2 inline-flex gap-2 rounded-md bg-[#ffffff] px-3 py-2 font-body text-sm font-medium text-cta md:text-base'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Acesse a página <LogIn strokeWidth={1.5} />
-        </Link>
-      ) : null}
-    </div>
-  );
-};
 
 // app/[...slug]/page.js
 
--- components/ProjectContent.tsx
+++ components/ProjectContent.tsx
@@ -6,11 +6,13 @@ import urlBuilder from '@/sanity/lib/sanity-image-builder';
 import { PortableText, PortableTextReactComponents } from '@portabletext/react';
 import { getImageDimensions } from '@sanity/asset-utils';
 import cn from 'clsx';
+import { motion } from 'framer-motion';
 
 import { project_section } from '@/components/design-system';
 
 interface PortfolioDataLayerProps {
   project: Project;
+  lang: 'pt' | 'en' | 'es';
 }
 
 const portable_text_components: Partial<PortableTextReactComponents> = {
@@ -57,19 +59,38 @@ const portable_text_components: Partial<PortableTextReactComponents> = {
   },
 };
 
-export default function PortfolioDataLayer({
+export default function ProjectContent({
   project,
+  lang,
 }: PortfolioDataLayerProps) {
+  const val =
+    lang === 'pt'
+      ? project.body?.ptRaw
+      : lang === 'en'
+        ? project.body?.enRaw
+        : project.body?.esRaw;
   return (
     <>
-      <PortableText
-        value={project.body?.ptRaw}
-        components={portable_text_components}
-      />
+      <motion.div
+        initial={{
+          opacity: 0,
+        }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true, amount: 0.1 }}
+        transition={{ duration: 0.3, ease: 'easeInOut', delay: 0.3 }}
+      >
+        <PortableText value={val} components={portable_text_components} />
+      </motion.div>
       <div className='mb-4 grid gap-4 md:mb-8 md:grid-cols-2'>
         <ul className='flex flex-col gap-4'>
           {project.image_column_1?.map((image, i) => (
-            <li
+            <motion.li
+              initial={{
+                opacity: 0,
+              }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true, amount: 0.1 }}
+              transition={{ duration: 0.3, ease: 'easeInOut' }}
               key={`column_1_image_${i}`}
               className='overflow-hidden rounded-2xl'
             >
@@ -80,12 +101,18 @@ export default function PortfolioDataLayer({
                 height={504}
                 quality={100}
               />
-            </li>
+            </motion.li>
           ))}
         </ul>
         <ul className='flex flex-col gap-4'>
           {project.image_column_2?.map((image, i) => (
-            <li
+            <motion.li
+              initial={{
+                opacity: 0,
+              }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true, amount: 0.1 }}
+              transition={{ duration: 0.3, ease: 'easeInOut' }}
               key={`column_2_image_${i}`}
               className='overflow-hidden rounded-2xl'
             >
@@ -96,7 +123,7 @@ export default function PortfolioDataLayer({
                 height={504}
                 quality={100}
               />
-            </li>
+            </motion.li>
           ))}
         </ul>
       </div>
